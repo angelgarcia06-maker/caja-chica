1d940cd6c64db39aa12c61a7e4d6bfef6b3ee223
angelgarcia06-maker <garciamenaangel06@gmail.com>
2026-04-23 22:29:42 -0500

    limpiar backend

--- backend/caja-chica-main/caja-chica-main/src/main/java/com/example/caja_chica/repository/CajaChicaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.caja_chica.repository;
-
-import com.example.caja_chica.model.CajaChica;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CajaChicaRepository extends JpaRepository<CajaChica, Long> {
-}
--- backend/caja-chica-main/caja-chica-main/src/main/java/com/example/caja_chica/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.caja_chica.repository;
-
-import com.example.caja_chica.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByUsername(String username);
-}
